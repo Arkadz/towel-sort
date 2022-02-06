ba8f26ec9e987c126afde6a4bc27589d31a3a38b
Arkadz <penkratpavel@gmail.com>
2022-02-06 15:11:20 +0300

    feat: add index.js solvation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    return matrix == null ? [] : matrix.reduce((acc, item, index) => {
+        item.sort((a, b) => index%2 ? b - a : a - b).map(elem => acc.push(elem));
+        return acc
+    }, []);
 }
